01520de24214a49c7b19a2032629ad58a4ad4184
Tiago-Goulart-dev <141263511+Tiago-Goulart-dev@users.noreply.github.com>
2024-02-09 14:54:10 -0300

    Upload Exercício

---
+++ exercicio.ts
@@ -0,0 +1,13 @@
+function multiplica(num1: number, num2:number): number {
+    let num3: number = num1 * num2;
+    console.log(num3);
+    return num3;
+}
+
+function oi(nome: string): string {
+    console.log("Olá " + nome);
+    return "Olá " + nome;
+}
+
+multiplica(10, 5);
+oi('Tiago');
